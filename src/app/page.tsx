3da7551001138a1908049f115a5146612c964086
BautistaOnorato <bautistaonorato27@gmail.com>
2025-07-21 14:13:42 -0300

    styles customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Home() {
   return (
-    <div>
-      <p className="text-blue-700 font-extrabold text-9xl border-b-8 border-yellow-300">
-        Aguante Boca
-      </p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant={"elevated"}>Button</Button>
+        </div>
+        <div>
+          <Input placeholder="Input" type="text" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea value={"Textarea"} />
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+      </div>
     </div>
   );
 }
